f03bba595832a230e1fc4e478b5bfa561574a30c
Ditriy Anishchenko <1348010@gmail.com>
2019-06-10 13:58:32 +0300

    valid CRUD

--- todo/todo_list/urls.py
+++ todo/todo_list/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from .views import home, create_task
+from .views import (
+    home,
+    create_task,
+    edit_task,
+    remove_task,
+)
 
 urlpatterns = [
 
     path('', home, name='home'),
     path('create/', create_task, name='create_task'),
+    path('edit/<int:task_id>/', edit_task, name='edit_task'),
+    path('remove/<int:task_id>/', remove_task, name='remove_task'),
 ]
--- todo/todo_list/views.py
+++ todo/todo_list/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task
 from .forms import TaskForm
 
@@ -19,9 +19,36 @@ def create_task(request):
         if form.is_valid():
             data = form.cleaned_data
             Task.objects.create(**data)
-
+            last_id = Task.objects.last().id if Task.objects.last() else 1
+            currently_created_task = Task.objects.last()
+            currently_created_task.update_id(last_id + 1)
             return redirect('home')
         else:
             errors = form.errors
             return HttpResponse(f'{errors}')
     return HttpResponse('Wrong request method')
+
+
+def edit_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    if request.method == 'GET':
+        context = {'form': TaskForm(initial={'content': task.content})}
+        return render(request, 'edit_task.html', context)
+    elif request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            Task.objects.filter(id=task_id).update(**data)
+            return redirect('home')
+        else:
+            errors = form.errors
+            return HttpResponse(errors)
+    else:
+        return HttpResponse('INVALID REQUEST')
+
+
+def remove_task(request, task_id):
+    Task.objects.get(id=task_id).delete()
+    for new_id, task in enumerate(Task.objects.all(), start=1):
+        task.update_id(new_id)
+    return redirect('home')
